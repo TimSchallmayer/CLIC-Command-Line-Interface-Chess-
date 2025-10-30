1ea554e81a2a6045a261cb41ece2b7c1e9769c59
TheHolyLarZarus <tomhtmlqdh@gmail.com>
2025-10-30 23:26:52 +0100

    main

--- chess.h
+++ chess.h
@@ -13,7 +13,7 @@ typedef struct {
 // Prototypen der Funktionen
 void draw_chessboard(Piece* pieces, bool istwhite);
 void create_pieces(Piece* pieces);
-bool introduction();
+char * introduction();
 bool valid_move(Piece piece, Piece* pieces, int origin_x, int origin_y, int position_x, int position_y);
-bool is_piece(int x, int y, Piece* pieces, bool is_white);
+bool is_piece(int x, int y, Piece* pieces, char* color);
 #endif
