fabc2c05e51fbe33a590e3b83b41a83ad48fa2ec
TheHolyLarZarus <tomhtmlqdh@gmail.com>
2025-11-02 19:42:43 +0100

    main

--- main.c
+++ main.c
@@ -8,17 +8,26 @@
 int main(int argc, char* argv[]) {
     system("powershell -Command \"[Console]::OutputEncoding = [System.Text.Encoding]::UTF8\"");
     Piece* pieces = malloc(32 * sizeof(Piece));
+    Piece* terminated_pieces = malloc(32 * sizeof(Piece));
+    Position passant;
+    int passantr_counter = 0;
+
     if (pieces == NULL) {
         fprintf(stderr, "Memory allocation failed\n");
         exit(EXIT_FAILURE);
     }
-    int zug_counter = 0;
+    int zug_counter = 1;
     int halbzug_counter = 0;
     char * play_color = introduction();
+
     create_pieces(pieces);
     draw_chessboard(pieces, play_color);
-    char* data = make_fen(pieces, play_color, zug_counter, halbzug_counter);
-    printf("%s", data);
+    char* data = make_fen(pieces, play_color, zug_counter, halbzug_counter, passant);
+    printf("\nFEN: %s\n", data);
+    API_call api_call;
+    api_call.fen = data;
+    api_call.depth = 12;
+    api_call.max_thinking_time = 5000;
     while (1) {
         char piece[7];     
         char position[3];   
@@ -52,6 +61,7 @@ int main(int argc, char* argv[]) {
         int origin_x = tolower(origin[0]) - 'a'; 
         int origin_y = tolower(origin[1]) - '1';
         Piece choosen_piece;
+        
         for (int i = 0; i < 32; i++)
         {
             if (pieces[i].x == origin_x && pieces[i].y == origin_y && (pieces[i].is_white == false && strcmp(play_color, "white") == 0 || pieces[i].is_white == true && strcmp(play_color, "black") == 0))
@@ -65,15 +75,49 @@ int main(int argc, char* argv[]) {
             printf("Invalid position! \n");
             continue;
         }
-        if (!valid_move(choosen_piece, pieces, origin_x, origin_y, position_x, position_y))
+        if (!valid_move(choosen_piece, pieces, origin_x, origin_y, position_x, position_y, passant))
         {
             printf("Invalid move!\n");
             continue;
         }
+        if (is_piece(position_x, position_y, pieces, "both") || position_x == passant.x && position_y == passant.y)
+        {
+            if (position_x == passant.x && passant.y == position_y && strcmp(choosen_piece.name, "pawn") == 0)
+            {
+                int index = which_piece(pieces, position_x, passant.y + (choosen_piece.is_white ? -1 : 1));
+                passant.y = -1;
+                passant.x = -1;
+            }
+            else {
+                int index = which_piece(pieces, position_x, position_y);
+                if (index != -1) {
+                    printf("%s captures %s\n", choosen_piece.name, pieces[index].name);
+                    terminate_piece(pieces[index]);
+                }
+            }
+
+        }
         
+        int i = which_piece(pieces, origin_x, origin_y);
+        printf("Moving %s to %s\n", pieces[i].name, position);
+        if (strcmp(pieces[i].name, "Pawn") == 0)
+        {
+            halbzug_counter = 0;
+            if (abs(position_y - origin_y) == 2) {
+                passant.x = origin_x;
+                passant.y = (origin_y + position_y) / 2;  
+            } else {
+                passant.x = -1;
+                passant.y = -1;
+            }
+        }
+        pieces[i].x = position_x;
+        pieces[i].y = position_y;
+        pieces[i].has_moved = true;
 
         if (piece[0] == 'r' || strcmp(piece, "rook") == 0) {
             printf("   Moving Rook to %s\n", position);
+
             if (strcmp(play_color, "black") == 0) {
                 if (pieces[0].x == origin_x && pieces[0].y == origin_y) {
                     pieces[0].x = position_x;
@@ -100,6 +144,13 @@ int main(int argc, char* argv[]) {
         }
         else if (tolower(piece[0]) == 'p' || strcmp(piece, "pawn") == 0) {
             halbzug_counter = 0;
+            if (abs(position_y - origin_y) == 2) {
+                passant.x = origin_x;
+                passant.y = (origin_y + position_y) / 2;  
+            } else {
+                passant.x = -1;
+                passant.y = -1;
+            }
             printf("   Moving Pawn to %s\n", position);
             if (strcmp(play_color, "black") == 0) {
                 for (int i = 8; i < 16; i++) {
@@ -205,7 +256,6 @@ int main(int argc, char* argv[]) {
                 }
             }
         }
-
         else {
             printf("Unknown piece, origin or position.\n");
             continue;
@@ -215,6 +265,13 @@ int main(int argc, char* argv[]) {
         halbzug_counter++;
         // Hier kommt noch der API coll
         zug_counter++;
+        if (!(abs(position_y - origin_y) == 2 && strcmp(piece, "pawn") == 0)) {
+            passant.x = -1;
+            passant.y = -1;
+        }
+
+    }
     free(pieces);
+    free(terminated_pieces);
     return 0;
 }
