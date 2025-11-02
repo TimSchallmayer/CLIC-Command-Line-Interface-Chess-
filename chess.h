7beb0754234d0b4b1330e7187076fdace796c349
TheHolyLarZarus <tomhtmlqdh@gmail.com>
2025-11-02 19:42:28 +0100

    main

--- chess.h
+++ chess.h
@@ -12,13 +12,27 @@ typedef struct {
     bool has_moved;
 } Piece;
 
+typedef struct {
+    int x;
+    int y;
+} Position;
+
+typedef struct {
+    char * fen;
+    int depth;
+    int max_thinking_time;
+
+} API_call;
+
 // Prototypen der Funktionen
 void draw_chessboard(Piece* pieces, char* color);
 void create_pieces(Piece* pieces);
 char * introduction();
-bool valid_move(Piece piece, Piece* pieces, int origin_x, int origin_y, int position_x, int position_y);
+bool valid_move(Piece piece, Piece* pieces, int origin_x, int origin_y, int position_x, int position_y, Position passant);
 bool is_piece(int x, int y, Piece* pieces, char* color);
-char * make_fen(Piece* pieces, char * color, int zug_counter, int halbzug_counter);
+char * make_fen(Piece* pieces, char * color, int zug_counter, int halbzug_counter, Position passant);
 bool is_castleling_possible(Piece king, Piece rook, Piece* pieces, char* color);
 bool is_sqare_attacked(int x, int y, Piece* pieces, char* color);
+void terminate_piece(Piece piece);
+int which_piece(Piece* pieces, int x, int y);
 #endif
